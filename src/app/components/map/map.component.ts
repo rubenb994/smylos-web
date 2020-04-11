bbdb33ee7965386745367f133d7f5de60f271773
Arturo <arthurvdwerf@hotmail.com>
2020-04-11 16:03:24 +0200

    First test implementation of map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,19 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as SvgPanZoom from 'svg-pan-zoom';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
+
+  public svgPanZoomMap: SvgPanZoom.Instance;
+  public readonly svgZoomFactor = 0.2;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public onButtonClick(): void {
-    console.log('Ruben was here');
+  ngAfterViewInit(): void {
+    const svgPanZoomOptions: SvgPanZoom.Options = {
+      // maxZoom: 10,
+      // minZoom: 0.5,
+      zoomScaleSensitivity: 0.05,
+      center: true
+      
+    }
+    this.svgPanZoomMap = SvgPanZoom('#smylos-map', svgPanZoomOptions);
   }
+
+  public onClickZoomIn(): void {
+    const newZoom = this.svgPanZoomMap.getZoom() + this.svgZoomFactor;
+    this.svgPanZoomMap.zoom(newZoom);
+  }
+
+  public onClickZoomOut(): void {
+    const newZoom = this.svgPanZoomMap.getZoom() - this.svgZoomFactor;
+    this.svgPanZoomMap.zoom(newZoom);
+  }
+
+  public onClickAmerica(): void {
+console.log("NorthAmerica");
+  }
+  
+
+ 
+
+  
 
 }
