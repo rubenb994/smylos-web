19db8e8d9ba4c5396c6ab1c436f30ae3326621f9
Arturo <arthurvdwerf@hotmail.com>
2020-04-12 17:28:14 +0200

    added logo, remove logo and toolbar at small screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-  
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'smylos-web';
-  
+  public displayToolbar = true;
+  public displayLogo = true;
+
+  ngOnInit(): void {
+
+  }
+
+  public onWindowResize(): void {
+    this.evaluateToolbarDisplay();
+    this.evaluateLogoDisplay();
+  }
+
+  public evaluateToolbarDisplay(): void {
+    if (window.innerWidth < 600) {
+      this.displayToolbar = false;
+    } else {
+      this.displayToolbar = true;
+    }
+  }
+
+  public evaluateLogoDisplay(): void {
+    if (window.innerWidth < 400) {
+      this.displayLogo = false;
+    } else {
+      this.displayLogo = true;
+    }
+  }
 }
 
