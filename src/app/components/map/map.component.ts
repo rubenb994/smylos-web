414b1bbd0573339bb6a0a6dbaba9d169ba37cd96
Arturo <arthurvdwerf@hotmail.com>
2020-04-13 17:50:43 +0200

    First test implementation of hover & click for map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -5,13 +5,16 @@ import * as SvgPanZoom from 'svg-pan-zoom';
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
-  
+
 })
 export class MapComponent implements OnInit, AfterViewInit {
 
   public svgPanZoomMap: SvgPanZoom.Instance;
   public readonly svgZoomFactor = 0.2;
   private readonly minZoom = 1;
+
+  public bigSmileOfficeDot;
+  public executiveOfficeDot;
 
   constructor() { }
 
@@ -20,15 +23,17 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     const svgPanZoomOptions: SvgPanZoom.Options = {
-      maxZoom: 2.0,
-      minZoom: 0.0,
+      maxZoom: 3.0,
+      minZoom: 1,
       zoomScaleSensitivity: 0.05,
       center: true
-      
     }
     this.svgPanZoomMap = SvgPanZoom('#smylos-map', svgPanZoomOptions);
+
+    this.bigSmileOfficeDot = document.getElementById('bigSmileDot');
+    this.executiveOfficeDot = document.getElementById('executiveOfficeDot');
   }
-  
+
   public onClickZoomIn(): void {
     const newZoom = this.svgPanZoomMap.getZoom() + this.svgZoomFactor;
     this.svgPanZoomMap.zoom(newZoom);
@@ -36,9 +41,24 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   public onClickZoomOut(): void {
     const newZoom = this.svgPanZoomMap.getZoom() - this.svgZoomFactor;
-    if(newZoom < this.minZoom) {
+    if (newZoom < this.minZoom) {
       return;
     }
     this.svgPanZoomMap.zoom(newZoom);
+  }
+
+  public onClickBigSmileOffice(): void {
+    this.deactiveAllLocations();
+    this.bigSmileOfficeDot.style.opacity = '1';
+  }
+
+  public onClickExecutiveOffice(): void {
+    this.deactiveAllLocations();
+    this.executiveOfficeDot.style.opacity = '1';
+  }
+
+  public deactiveAllLocations(): void {
+    this.bigSmileOfficeDot.style.opacity = '0';
+    this.executiveOfficeDot.style.opacity = '0';
   }
 }
