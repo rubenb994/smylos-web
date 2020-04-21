1c3a54b793ffde13a15ca0723d19b59b5f537224
Ruben <r.brugman@student.fontys.nl>
2020-04-21 11:57:14 +0200

    Implemented auto player audio & added box shadows for chat

--- src/app/components/audio-button/audio-button.component.ts
+++ src/app/components/audio-button/audio-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { Audio } from 'src/app/models/audio';
 
 @Component({
@@ -6,39 +6,47 @@ import { Audio } from 'src/app/models/audio';
   templateUrl: './audio-button.component.html',
   styleUrls: ['./audio-button.component.scss']
 })
-export class AudioButtonComponent implements OnInit, OnChanges {
+export class AudioButtonComponent implements OnInit, OnChanges, AfterViewInit {
 
   @Input() audio: Audio;
+  @Input() autoPlay = false;
   @Output() audioCompleted = new EventEmitter<Audio>(null);
 
-  constructor() { }
+  public audioPlayerSrc: string;
+
+  public paused = true;
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
 
   }
 
   ngOnChanges(): void {
-    const audioPlayer = document.getElementById('audio-player');
+    if (this.audio == null) {
+      return;
+    }
+
+    this.audioPlayerSrc = this.getSrcUrl();
+  }
+
+  ngAfterViewInit(): void {
+    const audioPlayer = document.getElementById('audio-player') as HTMLAudioElement;
     if (audioPlayer == null) {
       return;
     }
+
+    if (this.autoPlay) {
+      this.onClickAudioPlayer(audioPlayer);
+      // Detect changes so that the audio button displays the right icon.
+      this.changeDetectorRef.detectChanges();
+    }
+
     // Event listener to check when the audio player has finished.
     audioPlayer.addEventListener('ended', () => {
       this.audioCompleted.emit(this.audio);
     });
-  }
-
-  /**
-   * Method to fetch the src url of a audio file.
-   */
-  public getSrcUrl(): string {
-    if (this.audio == null) {
-      return '';
-    }
-    // Thanks to Andre his weird json this was nessecary since the internal id's use underscores and a lowercase letter.
-    // While the audio files are using dots and a capital letter.
-    const audioId = this.audio.audio_id.replace('a', 'A').replace('_', '.').replace('_', '.');
-    return `assets/audio/${audioId}.mp3`;
   }
 
   /**
@@ -55,6 +63,13 @@ export class AudioButtonComponent implements OnInit, OnChanges {
     this.audioCompleted.emit(this.audio);
   }
 
+  public checkIfAudioPlayerIsPaused(audioPlayer: HTMLAudioElement): boolean {
+    if (audioPlayer == null) {
+      return false;
+    }
+    return audioPlayer.paused;
+  }
+
   /**
    * Method which triggers when the finish audio button is pressed.
    * Calls the output event.
@@ -63,4 +78,16 @@ export class AudioButtonComponent implements OnInit, OnChanges {
     this.audioCompleted.emit(this.audio);
   }
 
+  /**
+   * Method to fetch the src url of a audio file.
+   */
+  private getSrcUrl(): string {
+    if (this.audio == null) {
+      return '';
+    }
+    // Thanks to Andre his weird json this was nessecary since the internal id's use underscores and a lowercase letter.
+    // While the audio files are using dots and a capital letter.
+    const audioId = this.audio.audio_id.replace('a', 'A').replace('_', '.').replace('_', '.');
+    return `assets/audio/${audioId}.mp3`;
+  }
 }
